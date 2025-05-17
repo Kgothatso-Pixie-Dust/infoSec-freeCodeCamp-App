8c715b964c07ff6481a8798dc8e10b15546ddddc
u20676736 <102977062+u20676736@users.noreply.github.com>
2025-05-17 15:04:48 +0200

    Modfied App and ReadMe, deleted package.json

--- myApp.js
+++ myApp.js
@@ -2,9 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 const PORT = process.env.PORT || 3030;
-app.listen(PORT, () => {
-  console.log(`Programmer Sercure App started on port ${PORT}`);
-});
+
 
 
 
@@ -60,7 +58,7 @@ app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 3030;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
